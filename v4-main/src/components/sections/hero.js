642861e74df61b4a0791b9d73fc002a8baeeec16
Anirudh Dabas <anirudhdabas7@gmail.com>
2025-12-18 21:17:46 -0500

    Update content and config

--- v4-main/src/components/sections/hero.js
+++ v4-main/src/components/sections/hero.js
@@ -60,13 +60,12 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Anirudh Dabas.</h2>;
+  const three = <h3 className="big-heading">I build things.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
+        I'm a first year Computer Science student at the University of Waterloo. Currently, I’m focused on building Stoody.
         at{' '}
         <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
           Upstatement
@@ -81,7 +80,7 @@ const Hero = () => {
       href="https://www.newline.co/courses/build-a-spotify-connected-app"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Check out Stoody!
     </a>
   );
 
