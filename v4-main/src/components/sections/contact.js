d18b5de6f22af59043899d8bfbcb75cd2d65f566
Anirudh Dabas <anirudhdabas7@gmail.com>
2026-01-15 09:10:04 -0500

    change

--- v4-main/src/components/sections/contact.js
+++ v4-main/src/components/sections/contact.js
@@ -59,10 +59,12 @@ const Contact = () => {
 
       <h2 className="title">Get In Touch</h2>
 
-      <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
-      </p>
+<p>
+  I’m always open to building impactful things with great teams.<br />
+  If you’re an employer interested in co-op or internship opportunities, or someone looking to collaborate on meaningful projects, feel free to reach out.<br />
+  I read every message and will do my best to respond.
+</p>
+
 
       <a className="email-link" href={`mailto:${email}`}>
         Say Hello
